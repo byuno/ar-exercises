987498344260fe5c89d739514a7e580d6349ef6a
Bobby Uno <robert.y.uno@gmail.com>
2020-04-01 15:49:46 +0000

    all done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Bobby", last_name: "Uno", hourly_rate: 60)
+@store1.employees.create(first_name: "Bobby", last_name: "Ano", hourly_rate: 600)
+@store1.employees.create(first_name: "Bobby", last_name: "Eno", hourly_rate: 6000)
+
+@store2.employees.create(first_name: "Bobby", last_name: "Ino", hourly_rate: 60000)
+@store2.employees.create(first_name: "Bobby", last_name: "Ono", hourly_rate: 600000)
+@store2.employees.create(first_name: "Bobby", last_name: "Yno", hourly_rate: 6000000)
+
+pp @store1.employees
+
+pp @store2.employees
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name"
+@user_input_store = $stdin.gets.chomp
+@new_store = Store.create(name: @user_input_store)
+@new_store.errors.messages.each { |error| puts "Error: #{error}"}
+
+
+
+
